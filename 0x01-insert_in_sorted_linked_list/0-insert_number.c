9507714e4335a666993770ae1663ac48ff2a00aa
Lilibeth Tabares <1762@holbertonschool.com>
2021-04-28 11:13:21 -0500

    Changing declaration

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -9,7 +9,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *current, *new = NULL;
 
-	if ((*head) == NULL || head == NULL)
+	if ((*head) != NULL || head != NULL)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
