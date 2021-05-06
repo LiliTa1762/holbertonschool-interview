bd254abd6a881877379a850830ce791fc9acc8d9
Lilibeth Tabares <1762@holbertonschool.com>
2021-05-05 19:35:02 -0500

    Adding default value to right and left

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - function to add a node in a BT
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
- * Return: return a pointer to the new node, or NULL on failure
+ * Return: pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 			return (NULL);
 
 	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 	node->parent = parent;
 
 		return (node);
