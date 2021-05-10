d58a43abc86650c2a27f1ff6bcbd0a9aa3f54eb2
Lilibeth Tabares <1762@holbertonschool.com>
2021-05-10 14:58:33 -0500

    minimum operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Method that calculates the fewest
+number of operations needed to result
+in exactly n H characters"""
+
+
+def minOperations(n):
+
+    if n <= 0:
+        return 0
+
+    if n <= 4:
+        return n
+
+    if n % 2 == 0:
+        x = int((n / 2) + 1)
+        return x
+
+    else:
+        x = int((n / 2) + 2)
+        return x
