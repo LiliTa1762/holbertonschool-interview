5c895905fc9940dc311498651f14c8f5812b7d7f
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-05 22:06:55 -0500

    checking outcome

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,29 +1,3 @@
-#include "search.h"
-
-/**
- * cp - checks if a value is between prev and tmp
- * @prev: first node
- * @tmp: last node
- * @value: value to search for
- * Return: node contains the value or NULL
- */
-skiplist_t *cp(skiplist_t *prev, skiplist_t *tmp, int value)
-{
-	while (prev != tmp->next)
-	{
-		if (prev->n == value)
-			return (prev);
-		prev = prev->next;
-	}
-	return (NULL);
-}
-/**
- * linear_skip - searches for a value in a sorted skip list of integers
- * @list: pointer to the head of the skip list to search in
- * @value: value to search for
- * Return: pointer on the first node where value is located
- */
-skiplist_t *linear_skip(skiplist_t *list, int value)
 #include <stdlib.h>
 #include <math.h>
 #include "search.h"
