91afe4bbf505e37078230406f3a164a5632281d7
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-12 21:03:18 -0500

    may the force be with you

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,28 +2,24 @@
 
 const request = require('request');
 
-const movieID = process.argv[2];
-
-const url = `https://swapi-api.hbtn.io/api/films/${movieID}`;
-
-request(url, (error, response, body) => {
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
-  }
-
-  const characters = JSON.parse(body).characters;
-
-  for (const char in characters) {
-    const url2 = `https://swapi-api.hbtn.io/api/people/${char}/`;
-    request(url2, (error, response, body) => {
-      if (error) {
-        console.log(error);
-      }
-
-      const names = JSON.parse(body).name;
-      if (names !== undefined) {
-        console.log(names);
-      }
-    });
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
   }
 });
