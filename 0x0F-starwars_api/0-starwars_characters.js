da51ce48a1ac2de3cafc3ed192f77464af84693d
Lilibeth Tabares <1762@holbertonschool.com>
2021-08-12 20:55:16 -0500

    may the force be with you

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/3';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  const characters = JSON.parse(body).characters;
+
+  for (const char in characters) {
+    const url2 = `https://swapi-api.hbtn.io/api/people/${char}/`;
+    request(url2, (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+
+      const names = JSON.parse(body).name;
+      if (names !== undefined) {
+        console.log(names);
+      }
+    });
+  }
+});
